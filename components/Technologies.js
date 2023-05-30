cb5bbb97587b1fd5d749976764ff80ff6da98191
mARUwTC <chriswong0621@gmail.com>
2023-05-30 11:51:48 +0800

    update

--- components/Technologies.js
+++ components/Technologies.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { useInView } from 'react-intersection-observer';
 
 import TechStyles from "../styles/techns.module.css";
-
 import Technologie from "./technologie";
 
 const Technologies = () => {
@@ -38,23 +39,60 @@ const Technologies = () => {
     },
   ]);
 
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+    triggerOnce: true
+  });
+
+  const variants = {
+    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, x: -50 }
+  };
+
+  const variants1 = {
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: 50 }
+  };
+
   return (
-    <div className={TechStyles.technologies} id="technologies">
-      <h1>Technologies</h1>
-      <p>
-        I&apos;ve worked with a range a technologies in the web development world.
-        From Back-end To Design.
-      </p>
+    <div className={TechStyles.technologies} id="skills">
+      <motion.h1
+        ref={ref}
+        initial="hidden"
+        animate={inView ? "visible" : "hidden"}
+        variants={variants}
+        transition={{ duration: 0.5 }}
+      >
+        Skills
+      </motion.h1>
+      <motion.p
+        ref={ref}
+        initial="hidden"
+        animate={inView ? "visible" : "hidden"}
+        variants={variants}
+        transition={{ delay: 0.25, duration: 0.5 }}
+      >
+        I&apos;m familiar with a range of skills in the IT industry. From Infrastructure to Development.
+      </motion.p>
 
       <div className={TechStyles.techgroup}>
         {tech.map((t) => {
           return (
-            <Technologie
-              title={t.title}
-              para={t.para}
-              tech={t.tech}
-              key={t.id}
-            />
+            <motion.div
+              key={tech}
+              ref={ref}
+              initial="hidden"
+              animate={inView ? "visible" : "hidden"}
+              variants={variants1}
+              transition={{ delay: 0.5, duration: 0.5 }}
+            >
+              <Technologie
+                title={t.title}
+                para={t.para}
+                tech={t.tech}
+                key={t.id}
+              />
+            </motion.div>
           );
         })}
       </div>
