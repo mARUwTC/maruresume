1804a18423fbb10e1a7b3e5fc7de67004d3c4448
mARUwTC <chriswong0621@gmail.com>
2023-06-02 00:02:31 +0800

    update pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 import React from "react";
 
-import HomeStyles from "../styles/Home.module.css";
+import HomeStyles from "../styles/home.module.css";
 import Technologies from "../components/Technologies";
 import Footer from "../components/Footer";
 import Experience from "../components/Exprience";
