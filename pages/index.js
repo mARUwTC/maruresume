0e17f8a6419a3bb16f501ac647346265ebba05d9
mARUwTC <chriswong0621@gmail.com>
2023-06-02 22:04:16 +0800

    update Exprience.js and pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,8 @@ export default function Home() {
             initial={{ scale: 0, opacity: 0 }}
             style={{ textAlign: "justify" }}
           >
-            Hello, I&apos;m Chris Wong, a System Engineer with three years of experience. I enjoy tackling complex problems and am dedicated to delivering high-quality work. Looking forward to collaborating with others in the industry.          </motion.p>
+            Hello, I&apos;m Chris Wong, a System Engineer with three years of experience. I enjoy tackling complex problems and am dedicated to delivering high-quality work.
+          </motion.p>
         </div>
         <div className={HomeStyles.more} onClick={ToExp}>
           <FontAwesomeIcon icon={faChevronDown} />
