a612dd039981132cd1065835affdc0491d1700ce
mARUwTC <chriswong0621@gmail.com>
2023-06-04 02:53:41 +0800

    update package.json and _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,32 +3,6 @@ import { AnimatePresence } from "framer-motion";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 
-import { MongoClient } from 'mongodb';
-
-const uri = 'mongodb+srv://chriswong0621:LBhi9OnxW8uOTYJA@marucluster.gpjswik.mongodb.net/getIP?retryWrites=true&w=majority';
-
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const logIp = async (req) => {
-  const ip = req.headers['x-real-ip'] || req.socket.remoteAddress;
-
-  const data = {
-    timestamp: new Date().toISOString(),
-    ip: ip
-  };
-
-  try {
-    await client.connect();
-    const database = client.db('getIP');
-    const collection = database.collection('ips');
-    await collection.insertOne(data);
-  } catch (err) {
-    console.error(err);
-  } finally {
-    await client.close();
-  }
-};
-
 const MyApp = ({ Component, pageProps, router }) => {
   return (
     <Layout>
@@ -38,10 +12,5 @@ const MyApp = ({ Component, pageProps, router }) => {
     </Layout>
   );
 }
-
-export const getServerSideProps = async (context) => {
-  await logIp(context.req);
-  return { props: {} };
-};
 
 export default MyApp;
