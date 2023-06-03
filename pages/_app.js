88ae263b46a5fb49926bcbd2df74df4883e1e355
mARUwTC <chriswong0621@gmail.com>
2023-06-03 22:31:51 +0800

    build(deps): update 3 files and delete 1 file

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,33 @@ import { AnimatePresence } from "framer-motion";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps, router }) {
+import { MongoClient } from 'mongodb';
+
+const uri = 'mongodb+srv://chriswong0621:LBhi9OnxW8uOTYJA@marucluster.gpjswik.mongodb.net/getIP?retryWrites=true&w=majority';
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const logIp = async (req) => {
+  const ip = req.headers['x-real-ip'] || req.socket.remoteAddress;
+
+  const data = {
+      timestamp: new Date().toISOString(),
+      ip: ip
+  };
+
+  try {
+      await client.connect();
+      const database = client.db('<database>');
+      const collection = database.collection('ips');
+      await collection.insertOne(data);
+  } catch (err) {
+      console.error(err);
+  } finally {
+      await client.close();
+  }
+};
+
+const MyApp = ({ Component, pageProps, router }) =>{
   return (
     <Layout>
       <AnimatePresence mode='wait'>
@@ -12,5 +38,10 @@ function MyApp({ Component, pageProps, router }) {
     </Layout>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  await logIp(context.req);
+  return { props: {} };
+};
 
 export default MyApp;
