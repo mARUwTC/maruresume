ae81f81ca461c754e2246e97b80f62e85d909b6c
mARUwTC <86964487+mARUwTC@users.noreply.github.com>
2023-06-08 15:30:31 +0800

    Update Exprience.js

--- components/Exprience.js
+++ components/Exprience.js
@@ -23,8 +23,7 @@ const starIcon = {
 function Experience() {
     const timeline = [
         { icon: workIcon, date: '2022 - present', title: 'System Engineer', desc: 'Networking, Security management, Infrastructure management' },
-        { icon: workIcon, date: 'DEV 2020 - JAN 2022', title: 'IT Support Officer', desc: 'One-man band IT officer' },
-        { icon: workIcon, date: 'JUN 2020 - NOV 2020', title: 'IT Officer', desc: 'System management, User support' },
+        { icon: workIcon, date: 'DEV 2020 - JAN 2022', title: 'IT Support Officer', desc: 'One-man band IT officer including System management, User support' },
         { icon: workIcon, date: 'SEP 2019 - MAY 2020', title: 'Engineer', desc: 'IT Project' },
         { icon: schoolIcon, title: 'Computing', subtitle: 'BSc (Hons)' },
         { icon: schoolIcon, title: 'Computer and Information Security', subtitle: 'Diploma' },
